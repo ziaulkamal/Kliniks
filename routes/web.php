57087ae61e7d552f4d984e1f6f659c5ad200cf09
Ziaul Kamal <codepad@usa.com>
2024-01-10 02:37:57 +0700

    delete junk component

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\DahsboardController;
+
 use Illuminate\Support\Facades\Route;
 
 
-
-
-
-
-Route::get('/{any}', [DahsboardController::class, 'handleRequest'])->where('any', '.*')->name('dynamic');
